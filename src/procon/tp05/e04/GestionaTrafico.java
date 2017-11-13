3dcfe6d20c7c87bcaef8bf04078428c2fc7fe3d8
Diego P. M. Baltar <dpmbaltar@gmail.com>
2017-11-13 02:05:49 -0300

    Falta corrección de DeadLock
    Se genera al pasar TOLRANCIA y liberar un lock del lado opuesto;
    si no hay del otro lado, pero si del mismo, debería avisar.

--- src/procon/tp05/e04/GestionaTrafico.java
+++ src/procon/tp05/e04/GestionaTrafico.java
@@ -3,6 +3,6 @@ package procon.tp05.e04;
 public interface GestionaTrafico {
     public void entrarCocheDelNorte(int id) throws InterruptedException;
     public void entrarCocheDelSur(int id) throws InterruptedException;
-    public void salirCocheDelNorte();
-    public void salirCocheDelSur();
+    public void salirCocheDelNorte() throws InterruptedException;
+    public void salirCocheDelSur() throws InterruptedException;
 }
--- src/procon/tp05/e04/GestionaTraficoConSemaforo.java
+++ src/procon/tp05/e04/GestionaTraficoConSemaforo.java
@@ -4,34 +4,83 @@ import java.util.concurrent.Semaphore;
 
 public class GestionaTraficoConSemaforo implements GestionaTrafico {
 
-    private int ultimosDesdeNorte = 0;
-    private int ultimosDesdeSur = 0;
+    private static int TOLERANCIA = 3;
+    private int esperanDesdeNorte = 0;
+    private int esperanDesdeSur = 0;
+    private int seguidosDesdeNorte = 0;
+    private int seguidosDesdeSur = 0;
+    private int coche = 0;
     private Semaphore desdeNorte = new Semaphore(1, true);
     private Semaphore desdeSur = new Semaphore(1, true);
     private Semaphore pasar = new Semaphore(1, true);
+    private Semaphore mutex = new Semaphore(1);
 
     @Override
     public void entrarCocheDelNorte(int id) throws InterruptedException {
-        // TODO Auto-generated method stub
-
+        System.out.println("Coche "+id+" llega desde el Norte.");
+        desdeNorte.acquire();
+        //System.out.println("Coche "+id+" es el siguiente desde el Norte.");
+        pasar.acquire();
+        mutex.acquire();
+        System.out.println("Coche "+id+" entra desde el Norte.");
+        coche = id;
+        seguidosDesdeNorte++;
+        seguidosDesdeSur = 0;
+        mutex.release();
     }
 
     @Override
     public void entrarCocheDelSur(int id) throws InterruptedException {
-        // TODO Auto-generated method stub
-
+        System.out.println("Coche "+id+" llega desde el Sur.");
+        desdeSur.acquire();
+        //System.out.println("Coche "+id+" es el siguiente desde el Sur.");
+        pasar.acquire();
+        mutex.acquire();
+        System.out.println("Coche "+id+" entra desde el Sur.");
+        coche = id;
+        seguidosDesdeSur++;
+        seguidosDesdeNorte = 0;
+        mutex.release();
     }
 
     @Override
-    public void salirCocheDelNorte() {
-        // TODO Auto-generated method stub
+    public void salirCocheDelNorte() throws InterruptedException {
+        mutex.acquire();
+        System.out.println("Coche "+coche+" sale desde el Norte.");
+        coche = 0;
 
+        if (seguidosDesdeNorte >= TOLERANCIA) {
+            System.out.println(TOLERANCIA+" seguidos desde el Norte. Le toca al Sur");
+            seguidosDesdeNorte = 0;
+            desdeSur.release();
+            //desdeNorte.release();
+        } else {
+            desdeNorte.release();
+            //desdeSur.release();
+        }
+
+        mutex.release();
+        pasar.release();
     }
 
     @Override
-    public void salirCocheDelSur() {
-        // TODO Auto-generated method stub
+    public void salirCocheDelSur() throws InterruptedException {
+        mutex.acquire();
+        System.out.println("Coche "+coche+" sale desde el Sur.");
+        coche = 0;
 
+        if (seguidosDesdeSur >= TOLERANCIA) {
+            System.out.println(TOLERANCIA+" seguidos desde el Sur. Le toca al Norte");
+            seguidosDesdeSur = 0;
+            desdeNorte.release();
+            //desdeSur.release();
+        } else {
+            desdeSur.release();
+            //desdeNorte.release();
+        }
+
+        mutex.release();
+        pasar.release();
     }
 
 }
