e500a575dfcc81a00f8e3262a4e6223584243d4e
Diego P. M. Baltar <dpmbaltar@gmail.com>
2017-11-13 00:02:39 -0300

    Implementación del ejercicio 6 del T.P. 3
    Agregado implementación con Monitor.

---
+++ src/procon/tp03/e06/Agente.java
@@ -0,0 +1,21 @@
+package procon.tp03.e06;
+
+import java.util.Random;
+
+public class Agente implements Runnable {
+
+    private SalaFumadores sala;
+    private Random r;
+
+    public Agente(SalaFumadores sala) {
+        this.sala = sala;
+        r = new Random();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            sala.colocar(r.nextInt(3)+1);
+        }
+    }
+}
---
+++ src/procon/tp03/e06/SalaFumadores.java
@@ -0,0 +1,8 @@
+package procon.tp03.e06;
+
+public interface SalaFumadores {
+
+    public void colocar(int ingrediente);
+    public void entraFumar(int id);
+    public void terminaFumar();
+}
