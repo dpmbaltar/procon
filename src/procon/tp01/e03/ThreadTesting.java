f8cb03f53635b4f426da63f144987d9a0f56a749
dpmbaltar <dpmbaltar@lenovo-g50>
2019-01-31 18:39:43 -0300

    Finalizado Ej. 03 - TP 01

--- src/procon/tp01/e03/ThreadTesting.java
+++ src/procon/tp01/e03/ThreadTesting.java
@@ -1,16 +1,21 @@
 package procon.tp01.e03;
 
+/**
+ * (a) Sin el código agregado entre (b), no se puede asegurar cual hilo
+ * terminará primero, si el del main() o miHilo.
+ */
 public class ThreadTesting {
     public static void main(String[] args) {
         Thread miHilo = new MiEjecucion();
         miHilo.start();
+        // - (b)
         // - Agregado para que se comporte de manera determinada
         try {
             miHilo.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        // -
+        // - (b)
         System.out.println("En el main");
     }
 }
