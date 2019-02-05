132d9c927bc3f9844dced7c50d5fae98ec1dddd6
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-02-04 23:29:01 -0300

    Agregado Ej. 01 - TP 04

---
+++ src/procon/tp04/e01/Ejercicio1.java
@@ -0,0 +1,22 @@
+package procon.tp04.e01;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String[] nombresEmpleados = { "Pablo", "Luis", "Andrea", "Pedro",
+                "Paula" };
+        Saludo hola = new Saludo();
+        Personal[] elPersonal = new Personal[6];
+        elPersonal[0] = new Personal(hola, "JEFE", 5);
+        for (int i = 1; i < 6; i++)
+            elPersonal[i] = new Personal(hola, nombresEmpleados[i - 1]);
+        for (int i = 0; i < 6; i++)
+            elPersonal[i].start();
+        try {
+            for (int i = 0; i < 6; i++)
+                elPersonal[i].join();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
---
+++ src/procon/tp04/e01/Saludo.java
@@ -0,0 +1,18 @@
+package procon.tp04.e01;
+
+public class Saludo {
+    
+    synchronized void esperarJefe(String empleado) {
+        try {
+            wait();
+            System.out.println(empleado + "> Buenos dias jefe!");
+        } catch (InterruptedException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    synchronized void saludoJefe() {
+        System.out.println("JEFE> Buenos dias!");
+        notify();
+    }
+}
