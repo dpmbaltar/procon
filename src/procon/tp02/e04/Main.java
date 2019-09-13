5321827a2bdffadf600f8e02eda6c428eabbd78c
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-09-13 11:25:34 -0300

    Actualizado TP 2 - E 4

--- src/procon/tp02/e04/Main.java
+++ src/procon/tp02/e04/Main.java
@@ -1,26 +1,28 @@
 package procon.tp02.e04;
 
 /**
- * Valores posibles sin exclusión mutua: 4, 6, 8.
+ * Valores posibles sin exclusión mutua: 4, 6, 7, 8.
  * Valores posibles con exclusión mutua: 7, 8.
  *
  * @author Diego P. M. Baltar {@literal <dpmbaltar@gmail.com>}
  */
 public class Main {
     public static void main(String[] args) {
-        Valor valor = new Valor();
-        Thread t1 = new Thread(new TareaA(valor));
-        Thread t2 = new Thread(new TareaB(valor));
+        for (int i = 0; i < 20; i++) {
+            Valor valor = new Valor();
+            Thread t1 = new Thread(new TareaA(valor));
+            Thread t2 = new Thread(new TareaB(valor));
 
-        try {
-            t1.start();
-            t2.start();
-            t1.join();
-            t2.join();
+            try {
+                t1.start();
+                t2.start();
+                t1.join();
+                t2.join();
 
-            System.out.println("Total: " + valor.getValor());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+                System.out.println(valor.getValor());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
