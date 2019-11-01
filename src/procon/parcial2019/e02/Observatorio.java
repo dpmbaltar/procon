d4270b75f997b783f41642f7363cb24115461492
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-11-01 15:50:28 -0300

    Actualizado Ej. 2 - parcial 2019 (semáforos)

--- src/procon/parcial2019/e02/Observatorio.java
+++ src/procon/parcial2019/e02/Observatorio.java
@@ -7,12 +7,12 @@ public class Observatorio {
     private static final int CAPACIDAD = 5;
 
     private final Semaphore mutex = new Semaphore(1);
-    private final Semaphore visitantes = new Semaphore(CAPACIDAD, true);
-    private final Semaphore personal = new Semaphore(CAPACIDAD, true);
-    private final Semaphore profesionales = new Semaphore(CAPACIDAD, true);
     private final Semaphore entraVisitante = new Semaphore(0);
     private final Semaphore entraPersonal = new Semaphore(0);
     private final Semaphore entraProfesional = new Semaphore(0);
+    private final Semaphore visitantes = new Semaphore(CAPACIDAD, true);
+    private final Semaphore personal = new Semaphore(CAPACIDAD, true);
+    //private final Semaphore profesionales = new Semaphore(CAPACIDAD, true);
 
     private int visitantesEsperando = 0;
     private int personalEsperando = 0;
@@ -20,19 +20,22 @@ public class Observatorio {
     private int visitantesAdentro = 0;
     private int personalAdentro = 0;
     private int profesionalesAdentro = 0;
-    private int totalVisitantes = 0;
-    private int totalPersonal = 0;
-    private int totalProfesionales = 0;
+//    private int totalVisitantes = 0;
+//    private int totalPersonal = 0;
+//    private int totalProfesionales = 0;
+    private boolean entroAlguien = false;
 
     public void entraVisitante(String nombre, boolean sillaDeRuedas) throws InterruptedException {
         mutex.acquire();
         // Entran visitantes, personal o profesionales según cual llegue primero
-        if (totalVisitantes == 0 && totalPersonal == 0 && totalProfesionales == 0)
+        if (!entroAlguien) {
+            entroAlguien = true;
             entraVisitante.release();
+        }
         visitantesEsperando++;
         mutex.release();
 
-         // Evita que entren más de lo admitido (CAPACIDAD)
+        // Evita que entren más de lo admitido (CAPACIDAD)
         if (sillaDeRuedas)
             visitantes.acquire(2);
         else
@@ -42,7 +45,7 @@ public class Observatorio {
         mutex.acquire();
         visitantesEsperando--;
         visitantesAdentro++;
-        totalVisitantes++;
+        //totalVisitantes++;
         System.out.println(String.format("Entra visitante %s (silla: %s)", nombre, String.valueOf(sillaDeRuedas)));
         entraVisitante.release(); // Dejar entrar otro visitante
         mutex.release();
@@ -55,11 +58,6 @@ public class Observatorio {
         visitantesAdentro--;
 
         if (visitantesEsperando == 0 && visitantesAdentro == 0) {
-            System.out.println("!!!NO HAY MÁS VISITANTES DENTRO NI ESPERANDO!!!");
-            System.out.println(String.format("Total de visitas: %s", totalVisitantes));
-            System.out.println(String.format("Visitantes esperando: %s", visitantesEsperando));
-            System.out.println(String.format("Personal esperando: %s", personalEsperando));
-            System.out.println(String.format("Profesionales esperando: %s", profesionalesEsperando));
             entraVisitante.acquire(); // No hay visitantes adentro ni esperando (adquiere de inmediato)
             // Dejar pasar a profesionales o personal primero, si hay esperando
             if (personalEsperando > 0)
@@ -81,8 +79,10 @@ public class Observatorio {
     public void entraPersonal(String nombre) throws InterruptedException {
         mutex.acquire();
         // Entran visitantes, personal o profesionales según cual llegue primero
-        if (totalVisitantes == 0 && totalPersonal == 0 && totalProfesionales == 0)
+        if (!entroAlguien) {
+            entroAlguien = true;
             entraPersonal.release();
+        }
         personalEsperando++;
         mutex.release();
 
@@ -92,7 +92,7 @@ public class Observatorio {
         mutex.acquire();
         personalEsperando--;
         personalAdentro++;
-        totalPersonal++;
+        //totalPersonal++;
         System.out.println(String.format("Entra personal %s", nombre));
         entraPersonal.release(); // Dejar entrar más personal
         mutex.release();
@@ -105,11 +105,6 @@ public class Observatorio {
         personalAdentro--;
 
         if (personalEsperando == 0 && personalAdentro == 0) {
-            System.out.println("!!!NO HAY MÁS PERSONAL DENTRO NI ESPERANDO!!!");
-            System.out.println(String.format("Total de visitas: %s", totalPersonal));
-            System.out.println(String.format("Visitantes esperando: %s", visitantesEsperando));
-            System.out.println(String.format("Personal esperando: %s", personalEsperando));
-            System.out.println(String.format("Profesionales esperando: %s", profesionalesEsperando));
             entraPersonal.acquire(); // No hay personal adentro ni esperando (adquiere de inmediato)
             // Dejar pasar a profesionales o visitantes primero, si hay esperando
             if (profesionalesEsperando > 0)
@@ -128,20 +123,22 @@ public class Observatorio {
     public void entraProfesional(String nombre) throws InterruptedException {
         mutex.acquire();
         // Entran visitantes, personal o profesionales según cual llegue primero
-        if (totalVisitantes == 0 && totalPersonal == 0 && totalProfesionales == 0)
+        if (!entroAlguien) {
+            entroAlguien = true;
             entraProfesional.release();
+        }
         profesionalesEsperando++;
         mutex.release();
 
-        profesionales.acquire(CAPACIDAD);
+        //profesionales.acquire(CAPACIDAD);
         entraProfesional.acquire();
 
         mutex.acquire();
         profesionalesEsperando--;
         profesionalesAdentro++;
-        totalProfesionales++;
+        //totalProfesionales++;
         System.out.println(String.format("Entra profesional %s", nombre));
-        entraProfesional.release(); // Deja entrar al siguiente profesional
+        //entraProfesional.release(); // Deja entrar al siguiente profesional
         mutex.release();
     }
 
@@ -152,12 +149,7 @@ public class Observatorio {
         profesionalesAdentro--;
 
         if (profesionalesEsperando == 0 && profesionalesAdentro == 0) {
-            System.out.println("!!!NO HAY MÁS PROFESIONALES DENTRO NI ESPERANDO!!!");
-            System.out.println(String.format("Total de visitas: %s", totalProfesionales));
-            System.out.println(String.format("Visitantes esperando: %s", visitantesEsperando));
-            System.out.println(String.format("Personal esperando: %s", personalEsperando));
-            System.out.println(String.format("Profesionales esperando: %s", profesionalesEsperando));
-            entraProfesional.acquire(); // No hay profesionales adentro ni esperando (adquiere de inmediato)
+            //entraProfesional.acquire(); // No hay profesionales adentro ni esperando (adquiere de inmediato)
             // Dejar pasar a profesionales o visitantes primero, si hay esperando
             if (visitantesEsperando > 0)
                 entraVisitante.release(); // Entra visitante
@@ -166,10 +158,12 @@ public class Observatorio {
             else if (profesionalesEsperando > 0)
                 entraProfesional.release(); // Entra profesional
             // Sino, entra el que siga primero
+        } else {
+            entraProfesional.release();
         }
 
         mutex.release();
-        profesionales.release(CAPACIDAD);
+        //profesionales.release(CAPACIDAD);
     }
 
 }
