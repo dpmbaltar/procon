2c71d7d948803128c92b82d25c90d80635a716ee
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-11-04 01:49:42 -0300

    Actualizado Ej. 4 - TP05 (semáforos)

--- src/procon/tp05/e04/Main.java
+++ src/procon/tp05/e04/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //GestionaTrafico gt = new GestionaTraficoMonitor();
         //GestionaTrafico gt = new GestionaTraficoCerrojo();
-        GestionaTrafico gt = new GestionaTraficoConSemaforo();
+        GestionaTrafico gt = new GestionaTraficoSemaforo();
         Thread[] trafico = new Thread[5];
 
         for (int i = 0; i < 5; i++)
