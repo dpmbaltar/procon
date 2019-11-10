568bc3a3f921c762bdb796756e15d62085995405
Diego P. M. Baltar <dpmbaltar@gmail.com>
2019-11-10 15:51:42 -0300

    Actualizado TPOF2019

---
+++ src/procon/tpof2019/Camioneta.java
@@ -0,0 +1,36 @@
+package procon.tpof2019;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Camioneta implements Runnable {
+
+    /**
+     * Instancia del parque.
+     */
+    private final Parque parque;
+
+    /**
+     * Constructor con el parque.
+     *
+     * @param parque el parque
+     */
+    public Camioneta(Parque parque) {
+        this.parque = parque;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (parque.estaAbierto()) {
+                parque.llevarBolsosAlFinal();
+//                Thread.sleep(ThreadLocalRandom.current().nextInt(5, 10) * 100);
+                parque.traerBolsosAlInicio();
+//                Thread.sleep(ThreadLocalRandom.current().nextInt(5, 10) * 100);
+            }
+        } catch (InterruptedException e) {
+            Logger.getLogger(Visitante.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+}
